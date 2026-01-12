2dce71a0c9fe1b7eed1312c93fb6ee8737d76837
Carolista <cjones.renaissancewoman@gmail.com>
2026-01-12 05:46:38 -0500

    added clock to manage fps and added circleshape class

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -19,6 +21,8 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        # print(dt)
 
 if __name__ == "__main__":
     main()
