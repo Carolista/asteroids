09bc0be5b065fbe206230c1c999d3465c3c5e844
Carolista <cjones.renaissancewoman@gmail.com>
2026-01-12 08:48:35 -0500

    allow for position and render updates using groups

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
 
     while True:
@@ -21,13 +26,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        player.update(dt)
+
+        # Update positions
+        updatable.update(dt)
+
+        # Render
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+
+        # Manage fps
         dt = clock.tick(60) / 1000
-        # print(dt)
 
 if __name__ == "__main__":
     main()
